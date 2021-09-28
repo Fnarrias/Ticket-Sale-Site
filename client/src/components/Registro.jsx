e6cee611b825cbd1217adc1d4959b5f056708809
Fnarrias <fernando.narrias@gmail.com>
2021-09-28 17:41:43 -0300

    Registro de usuarios funcionando

--- client/src/components/Registro.jsx
+++ client/src/components/Registro.jsx
@@ -3,6 +3,8 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { Avatar, Button, TextField } from "@mui/material";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import registro from "../actions/Registro";
+
 
 const Registro = () => {
   const paperStyle = {
@@ -14,6 +16,44 @@ const Registro = () => {
   const avatarStyle = { backgroundColor: "#d83f87" };
   const textFieldStyle = { margin: 5 };
   const buttonStyle = { marginTop: 25 };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    console.log(e.target);
+    const payload = {};
+    for (const [campo, val] of formData.entries()) payload[campo] = val;
+    const user = {
+      nombre: payload.nombre,
+      email: payload.email,
+      password: payload.password1,
+    };
+    console.log(user);
+
+    const crearUsuario = async () => {
+      const { success, message } = await registro(user);
+
+      console.log({ success });
+
+      if (success) {
+        form
+          .querySelectorAll(`input:not([type="submit"])`)
+          .forEach((input) => (input.value = ""));
+        window.alert("Usuario creado con éxito");
+      } else {
+        window.alert(`No se pudo registrar. ${message}`);
+      }
+    };
+    if(payload.password1 === payload.password2){
+        crearUsuario();
+    }else{
+      window.alert("Passwords no coinciden");
+    }
+    
+  };
+
+
   return (
     <>
       <Grid>
@@ -24,10 +64,12 @@ const Registro = () => {
             </Avatar>
             <h2>Regístrate</h2>
           </Grid>
+          <form onSubmit={handleSubmit}>
           <TextField
             style={textFieldStyle}
             variant="standard"
             label="Nombre"
+            name="nombre" 
             placeholder="Ingrese su nombre"
             fullWidth
             required
@@ -36,6 +78,7 @@ const Registro = () => {
             style={textFieldStyle}
             variant="standard"
             label="Correo"
+            name="email"
             placeholder="Ingrese su correo"
             fullWidth
             required
@@ -44,6 +87,7 @@ const Registro = () => {
             style={textFieldStyle}
             variant="standard"
             label="Contraseña"
+            name="password1"
             placeholder="Ingrese su contraseña"
             type="password"
             fullWidth
@@ -53,6 +97,7 @@ const Registro = () => {
             style={textFieldStyle}
             variant="standard"
             label="Contraseña2"
+            name="password2"
             placeholder="Repita su contraseña"
             type="password"
             fullWidth
@@ -67,6 +112,7 @@ const Registro = () => {
           >
             Crear cuenta
           </Button>
+          </form>
         </Paper>
       </Grid>
     </>
